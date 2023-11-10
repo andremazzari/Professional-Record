b4c93dc9c157a9e759e5fb3ff3a0ee15bbf4a713
Andre Saugo Mazzari <mazzariandre@gmail.com>
2023-11-10 19:54:19 -0300

    Update operation

--- assets/js/components/profissionais.js
+++ assets/js/components/profissionais.js
@@ -4,6 +4,7 @@ class Profissionais {
         this.lista_profissioais = [];
         this.max_id = 0;
         this.map_index_id = [];
+        this.linguagens = ['', 'Javascript', 'PHP', 'Java', 'Golang', 'Python', 'C#', 'C++'];
     }
 
     getProfissionais() {
@@ -40,8 +41,77 @@ class Profissionais {
         row.insertCell(3).innerText = dados_profissional.linguagem;
         //experiencia
         row.insertCell(4).innerText = dados_profissional.experiencia;
+        //editar
+        row.insertCell(5).innerHTML = `<div class="botao-editar botao-linha" onclick="editar_profissional(${this.max_id})"><span class="simbolo-botao-linha material-symbols-outlined">edit</span></div>`
         //excluir
-        row.insertCell(5).innerHTML = `<div class="botao-excluir" onclick="excluir_profissional(${this.max_id})"><span class="simbolo-excluir material-symbols-outlined">close</span></div>`;
+        row.insertCell(6).innerHTML = `<div class="botao-excluir botao-linha" onclick="excluir_profissional(${this.max_id})"><span class="simbolo-botao-linha material-symbols-outlined">close</span></div>`;
+    }
+
+    construir_select(linguagem_selecionada) {
+        let html_string = '<select id="atualizar-linguagem" class="input-atualizar">';
+
+        for (let index in this.linguagens) {
+            html_string += `\n<option value="${this.linguagens[index]}"`;
+            if (this.linguagens[index] == linguagem_selecionada) {
+                html_string += ' selected';
+            }
+            html_string += `>${this.linguagens[index]}</option>`;
+        }
+
+        html_string += '\n</select>';
+
+        return html_string;
+    }
+
+    linha_editar(id_profissional) {
+        let index = this.map_index_id.indexOf(id_profissional);
+        let dados_profissional = this.lista_profissioais[index];
+
+        let row = document.getElementById('profissional-' + (id_profissional).toString())
+
+        //nome
+        row.cells[0].innerHTML = `<input value="${dados_profissional.nome}" id="atualizar-nome" class="input-atualizar" type="text"/>`;
+        //sobrenome
+        row.cells[1].innerHTML = `<input value="${dados_profissional.sobrenome}" id="atualizar-sobrenome" class="input-atualizar" type="text"/>`;
+        //idade
+        row.cells[2].innerHTML = `<input value="${dados_profissional.idade}" id="atualizar-idade" class="input-atualizar" type="number" min="0"/>`;
+        //linguagem
+        row.cells[3].innerHTML = this.construir_select(dados_profissional.linguagem);
+        //experiencia
+        row.cells[4].innerHTML = `<input value="${dados_profissional.experiencia}" id="atualizar-experiencia" class="input-atualizar" type="number" min="0"/>`;
+        //confirmar edicao
+        row.cells[5].innerHTML = `<div class="botao-confirmar-edicao botao-linha" onclick="confirmar_edicao(${id_profissional})"><span class="simbolo-botao-linha material-symbols-outlined">check</span></div>`
+        //cancelar edicao
+        row.cells[6].innerHTML = `<div class="botao-cancelar-edicao botao-linha" onclick="sair_da_edicao(${id_profissional})"><span class="simbolo-botao-linha material-symbols-outlined">replay</span></div>`;
+    }
+
+    sair_da_edicao(id_profissional) {
+        let index = this.map_index_id.indexOf(id_profissional);
+        let dados_profissional = this.lista_profissioais[index];
+
+        let row = document.getElementById('profissional-' + (id_profissional).toString())
+
+        //nome
+        row.cells[0].innerText = dados_profissional.nome;
+        //sobrenome
+        row.cells[1].innerText = dados_profissional.sobrenome;
+        //idade
+        row.cells[2].innerText = dados_profissional.idade;
+        //linguagem
+        row.cells[3].innerText = dados_profissional.linguagem;
+        //experiencia
+        row.cells[4].innerText = dados_profissional.experiencia;
+        //editar
+        row.cells[5].innerHTML = `<div class="botao-editar botao-linha" onclick="editar_profissional(${id_profissional})"><span class="simbolo-botao-linha material-symbols-outlined">edit</span></div>`
+        //excluir
+        row.cells[6].innerHTML = `<div class="botao-excluir botao-linha" onclick="excluir_profissional(${id_profissional})"><span class="simbolo-botao-linha material-symbols-outlined">close</span></div>`;
+    }
+
+    confirmar_edicao(dados_atualizados, id_profissional) {
+        let index = this.map_index_id.indexOf(id_profissional);
+        
+        this.lista_profissioais[index] = dados_atualizados;
+        sair_da_edicao(id_profissional);
     }
 
     deleteProfissional(id) {
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var profissionais = new Profissionais();
 
 function atualiza_tabela() {
     //Obtem valores do formulario
-    var dados_profissional = {
+    let dados_profissional = {
         nome: document.getElementById('form-nome').value,
         sobrenome: document.getElementById('form-sobrenome').value,
         idade: document.getElementById('form-idade').value,
@@ -19,10 +19,32 @@ function atualiza_tabela() {
     //profissionais.constroi_lista();
 }
 
+function editar_profissional(id_profissional) {
+    profissionais.linha_editar(id_profissional);
+}
+
 function excluir_profissional(id_profissional) {
     //remove profissional da lista
     profissionais.deleteProfissional(id_profissional);
     //remove linha da tabela
     let row = document.getElementById(`profissional-${id_profissional}`);
     row.parentNode.removeChild(row);
+}
+
+function sair_da_edicao(id_profissional) {
+    profissionais.sair_da_edicao(id_profissional);
+}
+
+function confirmar_edicao(id_profissional) {
+    //Obtem valores de atualização
+    let dados_atualizados = {
+        nome: document.getElementById('atualizar-nome').value,
+        sobrenome: document.getElementById('atualizar-sobrenome').value,
+        idade: document.getElementById('atualizar-idade').value,
+        linguagem: document.getElementById('atualizar-linguagem').value,
+        experiencia: document.getElementById('atualizar-experiencia').value,
+        id: id_profissional
+    }
+
+    profissionais.confirmar_edicao(dados_atualizados, id_profissional);
 }
